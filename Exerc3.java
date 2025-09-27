791ca09358af0e2b5f2af78497b1a8778c41c63c
JoshuaAug <joaopedrobrasilportugal@gmail.com>
2025-09-26 23:16:43 -0300

    Update Exerc3.java

--- Exerc3.java
+++ Exerc3.java
@@ -5,22 +5,24 @@ import java.util.Objects;
 public class Exerc3 {
     public static void main(String[] args) {
 
+        // Declaração de scanner e variaveis
         Scanner sc = new Scanner(System.in);
         String auxiliar;
         ArrayList<String> tarefas = new ArrayList<>();
 
+        // Solicitação de quantas tarefas
         System.out.print("Quantas tarefas realizaras?: ");
         int qntTarefas = sc.nextInt();
         sc.nextLine();
 
+        // De acordo com a quantidade vai pedindo as tarefas
         for (int i = 0; i < qntTarefas; i++) {
             System.out.printf("Tarefa %d: ", i+1);
             auxiliar = sc.nextLine();
             tarefas.add(auxiliar);
-            // ""Limpa"" o leitor
-            // sc.nextLine();
         }
 
+        // Printa as tarefas enumeradas
         System.out.print("-------- Tarefas --------\n");
         for (int i = 0; i < qntTarefas; i++) {
             System.out.printf("%dº tarefa: %s\n", i+1, tarefas.get(i));
