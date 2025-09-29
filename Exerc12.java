041d5bf0a20f31a8503043633d54b1fb43ba2f59
JoshuaAug <joaopedrobrasilportugal@gmail.com>
2025-09-29 19:03:31 -0300

    Update Exerc12.java

--- Exerc12.java
+++ Exerc12.java
@@ -4,7 +4,7 @@ public class Exerc12 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         boolean valor=false;
-        char clima;
+        char clima, estacao='p';
 
         do {
             System.out.print("[S] Sol\n");
@@ -14,31 +14,100 @@ public class Exerc12 {
             clima = sc.next().toUpperCase().charAt(0);
 
             if (clima == 'S' || clima == 'C'){
-                valor = true;
+                break;
             } else {
                 System.out.print("Clima inválido");
             }
-        } while (valor == false);
+        } while (true);
 
         System.out.print("Qual a temperatura de hoje? ");
         int temperatura = sc.nextInt();
 
-        // com sol
-        // sol e - de 20
-        if (clima == 'S' && temperatura <= 20) {
-            System.out.print("Leve um casaco caso faça mais frio");
+        do {
+            System.out.print("[P] Primavera\n");
+            System.out.print("[V] Verão\n");
+            System.out.print("[O] Outono\n");
+            System.out.print("[I] Inverno\n");
 
-        // sol e + de 21
-        } else if (clima == 'S' && temperatura > 20) {
-            System.out.print("Use roupas frescas, a previsão é de calor");
+            System.out.print("Qual a estação?: ");
+            estacao = sc.next().toUpperCase().charAt(0);
 
-        // chuva
-        // chuva e - de 20
-        } else if (clima == 'C' && temperatura <= 20) {
-            System.out.print("Leve guarda chuva e roupas quentes");
-        // chuva e - de 20
-        } else {
-            System.out.print("Leve guarda chuva e roupas mais amenas");
+            if (estacao == 'P' || estacao == 'V' || estacao == 'O' || estacao == 'I'){
+                break;
+            } else {
+                System.out.print("Clima inválido");
+            }
+        } while (true);
+
+
+        switch(estacao){
+            // Primavera
+            case 'P':
+                // Com sol e - de 20ºC
+                if (clima == 'S' && temperatura <= 20){
+                    System.out.print("Use uma blusa e calça\n");
+                // Com sol e + de 20ºC
+                } else if (clima == 'S' && temperatura > 20){
+                    System.out.print("Use uma camisa e shorts\n");
+                // Com chuva e - de 20ºC
+                } else if (clima == 'C' && temperatura <= 20){
+                    System.out.print("Use duas blusas, calça e leve o guarda-chuva\n");
+                // Com chuva e + de 20ºC
+                } else {
+                    System.out.print("Use uma blusas, calça e leve o guarda-chuva\n");
+                }
+                break;
+            // Verão
+            case 'V':
+                // Com sol e - de 20ºC
+                if (clima == 'S' && temperatura <= 20){
+                    System.out.print("Use uma blusa e calça\n");
+                // Com sol e + de 20ºC
+                } else if (clima == 'S' && temperatura > 20){
+                    System.out.print("Use uma camisa e shorts\n");
+                // Com chuva e - de 20ºC
+                } else if (clima == 'C' && temperatura <= 20){
+                    System.out.print("Use uma blusa, calça e leve o guarda-chuva\n");
+                // Com chuva e + de 20ºC
+                } else {
+                    System.out.print("Use uma camisa, calça e leve o guarda-chuva\n");
+                }
+                break;
+            // Outono
+            case 'O':
+                // Com sol e - de 20ºC
+                if (clima == 'S' && temperatura <= 20){
+                    System.out.print("Use duas blusas e calça, considere usar uma touca\n");
+                // Com sol e + de 20ºC
+                } else if (clima == 'S' && temperatura > 20){
+                    System.out.print("Use uma blusa e calça\n");
+                // Com chuva e - de 20ºC
+                } else if (clima == 'C' && temperatura <= 20){
+                    System.out.print("Use duas/três blusas, calça, considere usar uma touca e leve o guarda-chuva\n");
+                // Com chuva e + de 20ºC
+                } else {
+                    System.out.print("Use uma/duas blusas, calça e leve o guarda-chuva\n");
+                }
+                break;
+            // Inverno
+            case 'I':
+                // Com sol e - de 20ºC
+                if (clima == 'S' && temperatura <= 20){
+                    System.out.print("Use três blusas, uma/duas calças, use uma touca e/ou cachecol\n");
+                // Com sol e + de 20ºC
+                } else if (clima == 'S' && temperatura > 20){
+                    System.out.print("Use duas blusas, uma calça e considere usar uma touca\n");
+                // Com chuva e - de 20ºC
+                } else if (clima == 'C' && temperatura <= 20){
+                    System.out.print("Use três/quatro blusas, duas calças, use uma touca e leve o guarda-chuva\n");
+                // Com chuva e + de 20ºC
+                } else {
+                    System.out.print("Use uma/duas blusas, calça, use uma touca e leve o guarda-chuva\n");
+                }
+                break;
         }
+
+
+
     }
 }
